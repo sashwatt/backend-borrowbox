da48be77682105076356bf13b067ef8363ba0e5f
sashwatt <supersashwat@gmail.com>
2025-03-01 08:41:34 +0545

    added product.js in models

--- models/product.js
+++ models/product.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    quantity: { type: Number, required: true },
-    status: { type: String, required: true, enum: ['Available', 'Low Stock', 'Out of Stock'] },
-    image: { type: String, required: false } // Store the image path
-  },
-  { timestamps: true }
-);
+// const productSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     description: { type: String, required: true },
+//     price: { type: Number, required: true },
+//     quantity: { type: Number, required: true },
+//     status: { type: String, required: true, enum: ['Available', 'Low Stock', 'Out of Stock'] },
+//     image: { type: String, required: false } // Store the image path
+//   },
+//   { timestamps: true }
+// );
 
-module.exports = mongoose.model("Product", productSchema);
+// module.exports = mongoose.model("Product", productSchema);
